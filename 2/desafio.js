01a2c7717f55947b1a1f5b1e7c6e55f9e7d1d972
o-paulo <paulo.pereira707@outlook.com>
2025-01-08 01:12:52 -0300

    feat: qual seu nome

---
+++ 2/desafio.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Quantos anos voce tem?")
+let areaEstudo = prompt("Qual linguagem de programacao voce esta estudando?")
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${areaEstudo}!`)
